88e744e20c446a2966f04163386254270395d454
Yaroslav <123>
2025-12-03 16:44:49 +0300

    фикс для railway

--- app.py
+++ app.py
@@ -1366,7 +1366,5 @@ def change_avatar():
     return jsonify({'success': True})
 
 if __name__ == "__main__":
-    # Для разработки
     port = int(os.environ.get("PORT", 5000))
-    debug_mode = os.environ.get("FLASK_ENV") == "development"
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(host='0.0.0.0', port=port)
